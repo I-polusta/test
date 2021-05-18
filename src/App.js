3fec3474cf7471afeefff5989878ca029c1f0bf3
Ishan Polusta <polustaishan2001@gmail.com>
2021-05-18 21:52:22 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React,{ useState } from "react";
+import './App.css';
+
+import Form from "./components/Form";
+import TodoList from "./components/TodoList";
+
+function App() {
+ 
+  return (
+    <div className="App">
+    
+    </div>
+  );
+}
+
+export default App;
